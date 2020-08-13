0c41a8eddd8ab3528ec964c747e550d82b7e886b
Andri <andri@localhost.localdomain>
2020-08-13 18:41:23 +0300

    launching

--- walk/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from django.contrib.auth.models import User,Group
-from walk.models import Post
-
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('created','title','status')
-
-admin.site.register(Post, PostAdmin)
--- walk/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WalkConfig(AppConfig):
-    name = 'walk'
