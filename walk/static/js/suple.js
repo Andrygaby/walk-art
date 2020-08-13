384cc451c5aa4766f5a013567cc77c5d6277bea7
Andri <andri@localhost.localdomain>
2020-08-13 18:51:19 +0300

    launching

---
+++ walk/static/js/suple.js
@@ -0,0 +1,36 @@
+// Get the modal for button 1
+function MyFuncOpen1(open) {
+	open = document.getElementById('id01').style.display='block'
+}
+
+// When the user clicks anywhere outside of the modal, close it
+function MyFuncClose1(close) {
+	close = document.getElementById('id01').style.display='none'
+}
+// Get the modal for button 1
+function MyFuncOpen2(open) {
+	open = document.getElementById('id02').style.display='block'
+}
+
+// When the user clicks anywhere outside of the modal, close it
+function MyFuncClose2(close) {
+	close = document.getElementById('id02').style.display='none'
+}
+
+// Get the modal for button 1
+function MyFuncOpen3(open) {
+	open = document.getElementById('id03').style.display='block'
+}
+// When the user clicks anywhere outside of the modal, close it
+function MyFuncClose3(close) {
+	close = document.getElementById('id03').style.display='none'
+}
+// Get the modal for button 1
+function MyFuncOpen4(open) {
+	open = document.getElementById('id04').style.display='block'
+}
+// When the user clicks anywhere outside of the modal, close it
+function MyFuncClose4(close) {
+	close = document.getElementById('id04').style.display='none'
+}
+
